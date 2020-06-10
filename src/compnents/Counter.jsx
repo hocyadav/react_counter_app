bd660a520ab233696ffbc4c52485ec265598fa81
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-10 16:56:25 +0530

    add class

--- src/compnents/Counter.jsx
+++ src/compnents/Counter.jsx
@@ -4,14 +4,11 @@ class Counter extends Component {
     state = {       //state as object
         count : 0
      }
-     styles = {     //styles as a object
-         fontSize: 50,
-         fontWeight: "bold"
-     }
+
     render() { 
         return ( 
         <div>
-            <span style={this.styles}>{this.formatCount()}</span>
+            <span className="badge badge-primary m-2">{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
