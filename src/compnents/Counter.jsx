a6f3ea622448dd945ce4d19b2240fa310acdcae1
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-10 16:58:35 +0530

    add class badge-warning

--- src/compnents/Counter.jsx
+++ src/compnents/Counter.jsx
@@ -8,7 +8,7 @@ class Counter extends Component {
     render() { 
         return ( 
         <div>
-            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span className="badge badge-warning m-2">{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
