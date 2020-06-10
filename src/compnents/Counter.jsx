e4b456d131fe2c976db412441aa14a381585be23
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-10 16:18:56 +0530

    New Module + call module

---
+++ src/compnents/Counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Hello</h1> );
+    }
+}
+export default Counter;
