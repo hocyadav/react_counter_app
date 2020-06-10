df442434723fc11d66c9e1596e054ac4a7916e4e
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-10 22:46:36 +0530

    add list of object

--- src/compnents/Counter.jsx
+++ src/compnents/Counter.jsx
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {       //state as object
-        count : 0
-     }
+        count : 0,
+        tagList : ["tag1","tag2","tag3"]
+     };
 
     render() { 
         return ( 
         <div>
             <span className={this.newMethod()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+        {this.state.tagList.map(tag => <li>{tag}</li>)}
+            </ul>
         </div>
         );
     }
