1e8023d020fb85fadd329b1a43f6d36b14db0f2f
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-10 23:00:07 +0530

    clean code

--- src/compnents/Counter.jsx
+++ src/compnents/Counter.jsx
@@ -17,17 +17,5 @@ class Counter extends Component {
         </div>
         );
     }
-
-    newMethod() {
-        let classes = "badge m-2 "; //common classes
-        classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
-        return classes;
-    }
-
-    formatCount(){
-        const {count} = this.state; //object destructuring
-        const jsxExpression = <h1>Zero</h1>;//we can pass JSX expression 
-        return count === 0 ? jsxExpression : count;//ctrl + d : multi select 
-    }
 }
 export default Counter;
