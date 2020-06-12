3516888b09f294b191eabb40d6db89322d3504c1
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-12 10:07:20 +0530

    increment button count

--- src/compnents/Counter.jsx
+++ src/compnents/Counter.jsx
@@ -14,19 +14,33 @@ class Counter extends Component {
     //      this.handleIncrement = this.handleIncrement.bind(this);
     //  }
 
+    
      handleIncrement = () => {
          console.log("Increment ",this);//able to access this, now we can update count variable
          this.state.count++;
          this.setState({count : this.state.count + 1});
      }
     render() { 
+
         return ( 
         <div>
 
-            <span className={classes}>{this.formatCount()}</span>
+            <span className={this.newMethod()}>{this.formatCount()}</span>
             <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
+    }
+
+    newMethod() {
+        let classes = "badge m-2 "; //common classes
+        classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
+        return classes;
+    }
+
+    formatCount(){
+        const {count} = this.state; //object destructuring
+        const jsxExpression = <h1>Zero</h1>;//we can pass JSX expression 
+        return count === 0 ? jsxExpression : count;//ctrl + d : multi select 
     }
 }
 export default Counter;
