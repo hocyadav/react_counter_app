c25d07b50a6c47707458b361423b066ab6ae138c
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-16 09:07:11 +0530

    create new component that will use to call multiple componts inside

---
+++ src/compnents/counters.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Counters extends Component {
+    state = {  }
+    render() { 
+        return ( <div>Counters components</div> );
+    }
+}
+ 
+export default Counters;
