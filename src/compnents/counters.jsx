a77a8733d39e4ebf914247b722848b7e610e09ab
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-16 09:09:22 +0530

    calling single component multiple times inside  new component

--- src/compnents/counters.jsx
+++ src/compnents/counters.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import Counter from "./counter";
 
 class Counters extends Component {
     state = {  }
     render() { 
-        return ( <div>Counters components</div> );
+        return ( 
+        <div>
+            <Counter />
+            <Counter />
+            <Counter />
+            <Counter />
+        </div> 
+        );
     }
 }
  
