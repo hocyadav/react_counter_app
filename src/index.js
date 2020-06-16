7c395559d01035a9ae389b81dc099c01f6cd4fa6
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-16 09:05:16 +0530

    file name change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from "./compnents/Counter";
+import Counter from "./compnents/counter";
 
 ReactDOM.render(
   <React.StrictMode>
