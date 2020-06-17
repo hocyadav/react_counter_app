2ae9ac6c7733e8d24ed997075cabd154076ebd8e
Hariom Yadav <hariom.yadav@broadcom.com>
2020-06-17 12:33:17 +0530

    access dynamic value inside h4 tag

--- src/compnents/Counter.jsx
+++ src/compnents/Counter.jsx
@@ -14,7 +14,7 @@ class Counter extends Component {
         console.log("props", this.props);
         return ( 
         <div>
-            {this.props.id}
+            <h4>{this.props.id}</h4>
             <span className={this.newMethod()}>{this.formatCount()}</span>
             <button onClick={ () => {this.handleIncrement({id:1})} } className="btn btn-secondary btn-sm">Increment</button>
         </div>
